79b77aebfbacd042109b290f3af55a82727d9973
edmeister <koed.dev@gmail.com>
2023-09-04 16:17:02 -0600

    update config files

---
+++ src/prisma/prisma.repository.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+// here we use extends keyword because we want to inherit the functionality
+// if we would have used implements we would have to manually create an implementation of each method
+// and we need our prisma service to implement the OnModuleInit in order to do operations with the DB
+export class PrismaService extends PrismaClient implements OnModuleInit {
+        //connect to prisma
+        async onModuleInit() {
+                await this.$connect();
+        }
+        //close connection when finished
+        async onModuleDestroy() {
+                await this.$disconnect();
+        }
+}
