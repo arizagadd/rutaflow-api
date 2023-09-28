e78f453637d6a299bd541357daef0f80d4a8f2f5
edmeister <koed.dev@gmail.com>
2023-09-28 13:35:09 -0700

    fix: convert incoming body string fields to number

--- src/route/dtos/route.dto.ts
+++ src/route/dtos/route.dto.ts
@@ -1,24 +1,28 @@
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateRouteDto {
     @IsNumber()
     @IsNotEmpty()
     @IsOptional()
-    routeTemplateId?: number;
+    @Transform(({ value }) => parseInt(value), { toClassOnly: true })
+    routeTemplateId: number;
 
     @IsNumber()
     @IsNotEmpty()
+    @Transform(({ value }) => parseInt(value), { toClassOnly: true })
     clientId: number;
 
     @IsOptional()
     @IsNumber()
     @IsNotEmpty()
+    @Transform(({ value }) => parseInt(value), { toClassOnly: true })
     driverId: number;
 
     @IsOptional()
     @IsNumber()
     @IsNotEmpty()
+    @Transform(({ value }) => parseInt(value), { toClassOnly: true })
     vehicleId: number;
 }
-
 export class RouteResponseDto {}
